cff7ccd4ba5aea7dd8dfa0f1bd3366bd5620771b
jaesung <cjs6027@naver.com>
2019-04-02 19:48:52 +0900

    Add : CallPhone Function

--- app/src/main/java/com/example/cjs60/scamcatch/Fragment/PhoneCallFragment.java
+++ app/src/main/java/com/example/cjs60/scamcatch/Fragment/PhoneCallFragment.java
@@ -1,23 +1,24 @@
 package com.example.cjs60.scamcatch.Fragment;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.cjs60.scamcatch.MainActivity;
 import com.example.cjs60.scamcatch.R;
 
-import org.w3c.dom.Text;
 
 public class PhoneCallFragment extends Fragment {
-    View view;
-    private Button one,two,three,four,five,six,seven,eight,nine,call,del; //레이아웃에 있는 버튼을 받기위한 변수 선언.
+    View view; //
+    private Button one,two,three,four,five,six,seven,eight,nine,zero,call,del; //레이아웃에 있는 버튼을 받기위한 변수 선언.
     private TextView callText; //레이아웃에 있는 번호적는 Text를 받기위한 변수 선언.
     private String Enum;
 
@@ -35,7 +36,7 @@ public class PhoneCallFragment extends Fragment {
     }
 
     //레이아웃의 객체들과 변수들을 연결하는 메소드. 항상 제일 먼저 선언되어야 한다.
-    private void SetLayout(View view){
+        private void SetLayout(View view){
         one = (Button)view.findViewById(R.id.onebtn);
         one.setOnClickListener(onClickListener);
         two = (Button)view.findViewById(R.id.twobtn);
@@ -54,6 +55,8 @@ public class PhoneCallFragment extends Fragment {
         eight.setOnClickListener(onClickListener);
         nine = (Button)view.findViewById(R.id.ninebtn);
         nine.setOnClickListener(onClickListener);
+        zero = (Button)view.findViewById(R.id.zerobtn);
+        zero.setOnClickListener(onClickListener);
         call = (Button)view.findViewById(R.id.callbtn);
         call.setOnClickListener(onClickListener);
         del = (Button)view.findViewById(R.id.delbtn);
@@ -66,44 +69,50 @@ public class PhoneCallFragment extends Fragment {
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.onebtn:
-                    callText.setText(callText.getText() + "1");
+                    callText.setText(callText.getText() + "1"); //기존것 + 숫자
                     break;
                 case R.id.twobtn:
-                    callText.setText(callText.getText() + "2");
+                    callText.setText(callText.getText() + "2"); //기존것 + 숫자
                     break;
                 case R.id.thirdbtn:
-                    callText.setText(callText.getText() + "3");
+                    callText.setText(callText.getText() + "3"); //기존것 + 숫자
                     break;
                 case R.id.fourbtn:
-                    callText.setText(callText.getText() + "4");
+                    callText.setText(callText.getText() + "4"); //기존것 + 숫자
                     break;
                 case R.id.fivebtn:
-                    callText.setText(callText.getText() + "5");
+                    callText.setText(callText.getText() + "5"); //기존것 + 숫자
                     break;
                 case R.id.sixbtn:
-                    callText.setText(callText.getText() + "6");
+                    callText.setText(callText.getText() + "6"); //기존것 + 숫자
                     break;
                 case R.id.sevenbtn:
-                    callText.setText(callText.getText() + "7");
+                    callText.setText(callText.getText() + "7"); //기존것 + 숫자
                     break;
                 case R.id.eigthbtn:
-                    callText.setText(callText.getText() + "8");
+                    callText.setText(callText.getText() + "8"); //기존것 + 숫자
                     break;
                 case R.id.ninebtn:
-                    callText.setText(callText.getText() + "9");
+                    callText.setText(callText.getText() + "9"); //기존것 + 숫자
+                    break;
+                case R.id.zerobtn:
+                    callText.setText(callText.getText() + "0"); //기존것 + 숫자
                     break;
                 case R.id.delbtn:
-                    String text = callText.getText().toString();
-                    if(text.length()>0)
-                        callText.setText(text.substring(0, text.length() - 1));
+                    String text = callText.getText().toString(); //이미 적힌 텍스트를 받는다.
+                    if(text.length()>0) //텍스트의 길이가 0 보다 작으면 지우지 않는다.
+                        callText.setText(text.substring(0, text.length() - 1)); //전체길이에서 1만 뺀다.
                     break;
                 case R.id.callbtn:
                     Enum = callText.getText().toString();
                     String tel = "tel:" + Enum;
-                    startActivity(new Intent("android.intent.action.CALL", Uri.parse(tel)));
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse(tel));
+                    startActivity(intent);
                     break;
             }
         }
     };
+
 
 }
--- app/src/main/java/com/example/cjs60/scamcatch/MainActivity.java
+++ app/src/main/java/com/example/cjs60/scamcatch/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.cjs60.scamcatch;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,14 +14,25 @@ import com.example.cjs60.scamcatch.Fragment.PhoneCallFragment;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //무조건 이거부터 실행됨.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //메인 액티비티에 레이아웃을 걸어준다.
+        PermissionCheck();
 
         PhoneCallFragment phoneCallFragment = new PhoneCallFragment(); //PhoneCallFragment로 넘어가기 위한 객체 생성
 
         //activity_main에 있는 main_frame에 phoneCallFragment를 씌운다
         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, phoneCallFragment).commit();
+    }
 
+    //권한있는지 없는지 확인
+    public void PermissionCheck(){
+        int permission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+        if(permission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},0);
+        }
+        else{
+            //권한있음.
+        }
     }
 }
